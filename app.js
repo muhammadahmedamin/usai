17160896dfaab354528759101cfb00e0b7d86da5
muhammadahmedamin <ahmedmuhammadaminhokai@gmail.com>
2023-05-17 23:05:12 +0500

    start

---
+++ app.js
@@ -0,0 +1,149 @@
+var firstName = "ahmed";
+alert(firstName);
+
+var lastName = "basit";
+alert(lastName);
+
+var email = "ahmed@example.com";
+alert(email);
+
+var phoneNumber = "123-456-7890";
+alert(phoneNumber);
+
+var password = "mySecretPassword";
+alert(password);
+
+alert("You're learning JavaScript!");
+alert("Hello! Welcome to my website.");
+var myVariable = "Hello";
+var myVariable;
+
+myVariable = "This is a string.";
+var teamName = "OpenAI Team";
+alert(teamName);
+
+var teamName = "OpenAI Team";
+alert(teamName);
+
+var bestMan = "ali";
+bestMan = "jaffar";
+alert(bestMan)
+
+var caseQty;
+caseQty = 144;
+alert(caseQty)
+
+var num = 9;
+var sum;
+sum = 5 + 3;
+alert(sum)
+
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+alert(orderTotal)
+
+var num = 5;
+num += 3;
+alert(num)
+
+var productCost = 3.45;
+alert(productCost)
+var nameOfBand;
+
+var myNumber = 42;
+alert(myNumber)
+var fullName = "JohnDoe";
+alert(fullName)
+
+var myVariable;
+var _underscoreVariable;
+var camelCaseVariable;
+var $dollarSignVariable;
+
+// var 123variable; // Cannot start with a number
+// var my-variable; // Cannot contain hyphens
+// var var; // Reserved keyword
+
+var largeNum = 1000 * 2000;
+alert(largeNum)
+
+var variable1 = 10;
+var variable2 = 5;
+var result = variable1 - variable2;
+alert(result)
+
+var remainder = 15 % 4;
+alert(remainder)
+
+var number1 = 5;
+var number2 = 10;
+var result = number1 * number2;
+alert(result);
+
+
+var calculatedNum = 2 + (2 * 6);
+console.log(calculatedNum); 
+
+var calculatedNum = (2 + 2) * 6;
+console.log(calculatedNum);
+
+var calculatedNum = (2 + 2) * (4 + 2);
+console.log(calculatedNum); 
+
+var calculatedNum = ((2 + 2) * 4) + 2;
+console.log(calculatedNum); 
+
+var cost = (2 + 2) * (4 + 10);
+console.log(cost); 
+
+var units = 2 + (2 * (4 + 10));
+console.log(units); 
+
+var pressure = (4 / 2) * 4;
+console.log(pressure); 
+
+var num = "2" + "2";
+console.log(num); 
+
+var message = "Hello," + "Dolly";
+alert(message); 
+
+alert("33" + 3); 
+
+var part1 = "Pakistan";
+var part2 = "Zindabad";
+var result = part1 + " " + part2;
+alert(result); 
+
+var string = "Hello";
+var number = 42;
+var result = string + number;
+console.log(result);
+
+var string1 = "Hello";
+var string2 = "World";
+var concatenated = string1 + string2;
+console.log(concatenated); 
+
+var firstName = prompt("Enter first name");
+
+var country = prompt("Country?", "China");
+
+var yourName = prompt("Enter Your Name");
+
+var response = prompt("Please enter your response", "Default input");
+
+var message = "Enter your favorite color";
+var defaultResponse = "Blue";
+var userResponse = prompt(message, defaultResponse);
+
+var message = "What is your favorite food?";
+var defaultResponse = "Pizza";
+var userResponse = prompt(message, defaultResponse);
+alert(userResponse);
+
+var city = "Karachi";
+if (city === "Karachi") {
+  console.log("The City of Lights");
+}
